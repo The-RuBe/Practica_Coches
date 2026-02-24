0a59e46befdda3859ad2782959b18c03cf8c510a
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-02-24 21:08:41 +0100

    Update Vehiculos.java

--- PracticaCoches/src/vehiculos/Vehiculos.java
+++ PracticaCoches/src/vehiculos/Vehiculos.java
@@ -1,9 +1,39 @@
 package vehiculos;
 
 public abstract class Vehiculos {
-	
+
 	private String marca;
 	private String modelo;
 	private String matricula;
-	
+
+	public Vehiculos(String marca, String modelo, String matricula) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
 }
