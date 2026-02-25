86a8fa350569ee4a9caccb289ca24632e7ece537
josegonz7 <jgoncre1412@g.educaand.es>
2026-02-25 13:52:56 +0100

    clase camion y main

--- PracticaCoches/src/vehiculos/Main.java
+++ PracticaCoches/src/vehiculos/Main.java
@@ -1,10 +1,19 @@
 package vehiculos;
 
+import vehiculos.camion.Camion;
+
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Camion camion = new Camion("MAN", "TGX", "7777ABC", 12000);
 
-	}
+        System.out.println("Demostración de polimorfismo con acelerar():");
 
+        Vehiculos v1 = camion;     
+
+        v1.acelerar();  
+        System.out.println("\nProbando método cargar() del camión:");
+        camion.cargar(8000);
+        camion.cargar(15000);
+    }
 }
--- PracticaCoches/src/vehiculos/camion/Camion.java
+++ PracticaCoches/src/vehiculos/camion/Camion.java
@@ -1,5 +1,52 @@
 package vehiculos.camion;
 
-public class Camion {
+import vehiculos.Vehiculos;
 
+public class Camion extends Vehiculos {
+	private int capacidadCarga;
+
+	public Camion(String marca, String modelo, String matricula, int capacidadCarga) {
+		super(marca, modelo, matricula);
+		this.capacidadCarga = capacidadCarga;
+	}
+
+	public int getCapacidadCarga() {
+		return capacidadCarga;
+	}
+
+	public void setCapacidadCarga(int capacidadCarga) {
+		this.capacidadCarga = capacidadCarga;
+	}
+
+	@Override
+	public String toString() {
+		return "Camion [capacidadCarga=" + capacidadCarga + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Camion other = (Camion) obj;
+		return capacidadCarga == other.capacidadCarga;
+	}
+
+	@Override
+	public void acelerar() {
+
+	}
+
+	public void cargar(double peso) {
+		if (peso <= 0) {
+			System.out.println("El peso debe ser mayor que 0 kg.");
+		} else if (peso <= capacidadCarga) {
+			System.out.println("Carga aceptada: " + peso + " kg.");
+		} else {
+			System.out.println("Carga rechazada, supera la capacidad máxima de " + capacidadCarga + " kg.");
+		}
+	}
 }
