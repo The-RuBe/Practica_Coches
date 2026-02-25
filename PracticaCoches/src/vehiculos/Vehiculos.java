57eb65230350514cfb60bb6cf003aad9f0b91d36
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-02-25 09:27:18 +0100

    Añadido código en la clase Vehiculos
    He añadido en la clase vehiculos el toString y los equals, además de el método abstracto de acelerar.

--- PracticaCoches/src/vehiculos/Vehiculos.java
+++ PracticaCoches/src/vehiculos/Vehiculos.java
@@ -1,5 +1,7 @@
 package vehiculos;
 
+import java.util.Objects;
+
 public abstract class Vehiculos {
 
 	private String marca;
@@ -35,5 +37,26 @@ public abstract class Vehiculos {
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
+
+	@Override
+	public String toString() {
+		return String.format("Vehiculos --> [ Marca= %s || Modelo= %s || Matrícula= %s ]", marca, modelo, matricula);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		Vehiculos other = (Vehiculos) obj;
+		return Objects.equals(marca, other.marca) && Objects.equals(matricula, other.matricula)
+				&& Objects.equals(modelo, other.modelo);
+	}
+
+	public abstract void acelerar();
 
 }
