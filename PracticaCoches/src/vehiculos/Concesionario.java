a960f1ffc902d031628c88e9864f39abba6c8d7b
Algalgom <agalgom1303@g.educaand.es>
2026-03-01 19:46:37 +0100

    Creado el metodo eliminarVehiculo
    Ha sido creado en la clase Concesionario

--- PracticaCoches/src/vehiculos/Concesionario.java
+++ PracticaCoches/src/vehiculos/Concesionario.java
@@ -20,6 +20,24 @@ public class Concesionario {
         }
     }
 
+    public void eliminarVehiculo(Vehiculo vehiculo) {
+        boolean encontrado = false;
+        for (int i = 0; i < numVehiculos && encontrado == false; i++) {
+            if (listaVehiculos[i].equals(vehiculo)) {
+                System.out.println("Vehículo eliminado: " + listaVehiculos[i].getMarca());
+                for (int j = i; j < numVehiculos - 1; j++) {
+                    listaVehiculos[j] = listaVehiculos[j + 1];
+                }
+                listaVehiculos[numVehiculos - 1] = null;
+                numVehiculos--;
+                encontrado = true;
+            }
+        }
+        if (encontrado == false) {
+            System.out.println("Error: No se encontró el vehículo " + vehiculo.getMarca());
+        }
+    }
+    
     public void mostrarInventario() {
         System.out.println("--- Inventario del Concesionario ---");
         for (int i = 0; i < numVehiculos; i++) {
