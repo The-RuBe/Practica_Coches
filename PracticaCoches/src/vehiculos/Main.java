c044c08d45c5fb0133c9da1e429730c589148c60
Migue-debug-coder <migueldiazromero4@gmail.com>
2026-03-02 13:18:15 +0100

    Métodos de añadir y eliminar vehiculo
    Probados en el Main y funcionan

--- PracticaCoches/src/vehiculos/Main.java
+++ PracticaCoches/src/vehiculos/Main.java
@@ -17,18 +17,33 @@ public class Main {
 		Sedan s1 = new Sedan("BMW", "Serie 3", "5555-EEE", "GASOLINA", 4);
 		Moto m1 = new Moto("Honda", "CBR 600", "7777-GGG", 600.0, "Deportiva");
 		Quad q1 = new Quad("Suzuki", "LTZ 400", "9999-III", 400.0, 1.80);
-		Camper cp1 = new Camper("Volkswagen", "California", "1212-KKL", "GRANDE", "Techo elevable y cocina");
+		Camper cp1 = new Camper("Volkswagen", "California", "1212-KKL", "L1H1", "Techo elevable y cocina");
 
 		System.out.println("Métodos acelerar():");
 
 		Vehiculo v1 = c1;
 
 		v1.acelerar();
-
+		System.out.println();
+		System.out.println("Método para añadir Vehículos al Array de Concesionario: ");
+		concesionario.añadirVehiculo(c1);
+		concesionario.añadirVehiculo(r1);
+		concesionario.añadirVehiculo(m1);
+		concesionario.añadirVehiculo(cp1);
+		concesionario.añadirVehiculo(q1);
+		concesionario.añadirVehiculo(s1);
+		
+		System.out.println();
+		
+		System.out.println("Método para eliminar Vehículo:");
+		concesionario.eliminarVehiculo(s1);
+		
+		System.out.println();
 		System.out.println("Método cargar() del camión:");
 		c1.cargar(8000);
 		c1.cargar(15000);
 		
+		System.out.println();
 		System.out.println("Método arrancar() del sedán:");
 		s1.arrancar(1);
 		s1.arrancar(1);
